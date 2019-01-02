b2bd53a2dd5b5d1f9882f7ffbbbfae2c89ba4abe
WangYuxiao <398088245@qq.com>
2019-01-02 10:14:37 +0800

    demo工程

---
+++ src/main/java/com/wyx/demo/TestSparkApplication.java
@@ -0,0 +1,14 @@
+package com.wyx.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSparkApplication.class, args);
+	}
+
+}
+
